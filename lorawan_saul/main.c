9e83724a176bed3271e485764fb3b6ccf1415a7f
Akshai M <akshai.m@fu-berlin.de>
2021-03-12 17:37:31 +0100

    Cayenne Registry

--- lorawan_saul/main.c
+++ lorawan_saul/main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2020 Freie Universität Berlin
+ *               2020 HAW Hamburg
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -15,6 +16,7 @@
  * @brief       GNRC LoRaWAN example application for RIOT
  *
  * @author      Akshai M   <akshai.m@fu-berlin.de>
+ *              José Alamos <jose.alamos@haw-hamburg.de>
  *
  * @}
  */
@@ -37,10 +39,7 @@
 
 #include "cayenne_lpp.h"
 
-static cayenne_lpp_t lpp;
-
 #define DELAY           (10 * US_PER_SEC)
-#define MODULE_DHT11
 
 #include "board.h"
 
@@ -52,11 +51,17 @@ static cayenne_lpp_t lpp;
 #include "net/gnrc/netreg.h"
 #include "net/loramac.h"
 
+#include "cayenne_lpp.h"
+
+#define DELAY           (10 * US_PER_SEC)
 #define LORAWAN_QUEUE_SIZE (4U)
+
+static cayenne_lpp_t lpp;
 
 // static gnrc_netif_t _netif;
 static uint8_t interface = 3;
 
+/*
 static void _print_buffer(const uint8_t *buffer, size_t len, const char *msg)
 {
     printf("%s: ", msg);
@@ -64,6 +69,7 @@ static void _print_buffer(const uint8_t *buffer, size_t len, const char *msg)
         printf("%02X", buffer[i]);
     }
 }
+*/
 
 int _send(void) 
 // int _send(const char *temp_s)
@@ -72,7 +78,13 @@ int _send(void)
     // char tmp[] = "\x10";
     uint8_t port = CONFIG_LORAMAC_DEFAULT_TX_PORT; /* Default: 2 */
 
-    pkt = gnrc_pktbuf_add(NULL, lpp.buffer,lpp.cursor, GNRC_NETTYPE_UNDEF);
+    cayenne_lpp_reset(&lpp);
+    msg_t msg;
+
+    if (!(pkt = gnrc_pktbuf_add(NULL, lpp.buffer, lpp.cursor, GNRC_NETTYPE_UNDEF))) {
+        puts("No space in packet buffer");
+        return 1;
+    }
 
     cayenne_lpp_reset(&lpp);
 
@@ -87,7 +99,6 @@ int _send(void)
     gnrc_netif_send(gnrc_netif_get_by_pid(interface), pkt); /* Forward the packet to LoRaWAN MAC for Uplink*/
 
     /* wait for packet status and check */
-    msg_t msg;
     msg_receive(&msg);
     if ((msg.type != GNRC_NETERR_MSG_TYPE) ||
         (msg.content.value != GNRC_NETERR_SUCCESS)) {
@@ -100,12 +111,10 @@ int _send(void)
     return 0;
 }
 
-static int _sense(char *_arg1)
+static int _sense(void)
 {
     phydat_t res;
     saul_reg_t *dev = saul_reg;
-
-    int temp = 0;
 
     if (dev == NULL) {
             puts("No SAUL devices present");
@@ -114,51 +123,41 @@ static int _sense(char *_arg1)
 
     while (dev) {
         int dim = saul_reg_read(dev, &res);
-        if (strcmp(saul_class_to_str(dev->driver->type), "ACT_SWITCH") &&
-            strcmp(saul_class_to_str(dev->driver->type), "SENSE_BTN") &&
-            strcmp(saul_class_to_str(dev->driver->type), "SENSE_HUM")) /*temporary filter */
+        if (!strcmp(saul_class_to_str(dev->driver->type), "SENSE_TEMP"))  /*temporary filter */
             {   
-                printf("\nDev: %s\tType: %s\n", dev->name,
-                        saul_class_to_str(dev->driver->type));
                 phydat_dump(&res, dim); 
                 printf("Dataa : %d, scale %d\n", res.val[0], res.scale);
-                temp = res.val[0];
+                cayenne_lpp_add_temperature(&lpp, 3, res.val[0]/10);
             }
-                dev = dev->next;
-        }
-        cayenne_lpp_add_temperature(&lpp, 3, temp/10);
-        size_t n = fmt_s16_dfp(_arg1, temp, -1);
+            dev = dev->next;
 
-        _print_buffer(lpp.buffer, lpp.cursor, "Result");
-        _arg1[n] = '\0';
-        printf("Temp Value : %s\n",_arg1);
+            //to do add additional sensors
+        }
         return 0;
 }
 
 /* join the network */
-static int _join(void)
+static netopt_enable_t _join(void)
 {
-    uint8_t enable = 1;
-    int status = 0;
+    netopt_enable_t enable = 1;
     gnrc_netapi_set(interface, NETOPT_LINK, 0, &enable, sizeof(enable));
     xtimer_usleep(DELAY);
-    gnrc_netapi_get(interface, NETOPT_LINK, 0, &status, sizeof(status));
-    return status;
+    gnrc_netapi_get(interface, NETOPT_LINK, 0, &enable, sizeof(enable));
+    return enable;
 }
 
 int main(void)
 {
     puts("LoRaWAN SAUL test application\n");
-    char arg1[5];
 
-    while(!_join()){}
+    while(_join() != NETOPT_ENABLE){}
 
     puts("Device joined\n");
 
     /* periodically sense and send sensor values */
     while (1) {
-        _sense(arg1);
-        printf("Sensed Value : %s\n",arg1);
+        _sense();
+        //printf("Sensed Value : %s\n",arg1);
         _send();
          xtimer_usleep(DELAY);
     }
