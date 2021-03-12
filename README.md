5aaa447e35881ba05d34df09422bdbf9e46aeeb8
Akshai M <akshai.m@fu-berlin.de>
2021-03-12 17:08:14 +0100

    saul_tests
    improve some parts
    
    add note regarding strcmp
    
    Cayenne Registry
    
    remove duplicated member
    
    add interface initialization
    
    Remove unnecessary lines
    
    Add git ignore
    
    minor nits
    
    Update interface initialisation
    
    Move puts to log_info & cleanup
    
    Update documentation
    
    Add JOIN_DELAY
    
    Update sleep timing
    
    Update doc
    
    Update Makefile
    
    Update README
    
    Update README
    
    Update readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# RIOT LoRaWAN Demo Application
+=============================
+
+## Preparations
+
+* Install [git](https://help.github.com/articles/set-up-git/), build tools (build-essential)
+, [dependencies](https://github.com/RIOT-OS/RIOT/wiki/Family:-native#dependencies), 
+[GCC Arm Embedded Toolchain](https://launchpad.net/gcc-arm-embedded), and
+[OpenOCD](https://github.com/RIOT-OS/RIOT/wiki/OpenOCD). Refer [here](https://github.com/RIOT-OS/Tutorials/blob/master/README.md) for more information.
+
+* Clone the repository to your local drive `git clone --recursive https://github.com/akshaim/riot_lorawan_demo`
+
+* Go to application. `cd riot_lorawan_demo/lorawan_saul`
+
+* Refer to [README](https://github.com/akshaim/riot_lorawan_demo/blob/saul/lorawan_saul/README.md) in application to run and customise the application in your target hardware.
--- lorawan_saul/main.c
+++ lorawan_saul/main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2020 Freie Universität Berlin
+ *               2020 HAW Hamburg
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -7,136 +8,159 @@
  */
 
 /**
- * @defgroup    lorawan_saul_example
+ * @defgroup    lorawan_saul_application
  * @brief       LoRaWAN SAUL application
  * @{
  *
  * @file
  * @brief       GNRC LoRaWAN example application for RIOT
  *
- * @author      Akshai M   <akshai.m@fu-berlin.de>
+ * @author      Akshai M    <akshai.m@fu-berlin.de>
+ *              José Alamos <jose.alamos@haw-hamburg.de>
  *
  * @}
  */
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "thread.h"
-#include "xtimer.h"
-
-// #include "timex.h"
-// #include "fmt.h"
-// #include "dht.h"
-// #include "dht_params.h"
-
-#include "phydat.h"
+/* SAUL registry interfaces */
 #include "saul_reg.h"
-#include <string.h>
 
-#define DELAY           (10 * US_PER_SEC)
-#define MODULE_DHT11
-
-#define MSG           "{ '%s': [ { 'ts': %llu000, 'values':{'%s': %d, '%s': %d}}]}"
-
-#include "board.h"
-
-#include "net/gnrc/netapi.h"
+/* GNRC's network interfaces */
 #include "net/gnrc/netif.h"
 
-#include "net/gnrc/pktbuf.h"
-#include "net/gnrc/pktdump.h"
-#include "net/gnrc/netreg.h"
-#include "net/loramac.h"
+/* Cayenne LPP interfaces, used to encode payload */
+#include "cayenne_lpp.h"
 
-#define LORAWAN_QUEUE_SIZE (4U)
+/* System logging header, offers "LOG_*" functions */
+#include "log.h"
 
-// static gnrc_netif_t _netif;
-static uint8_t interface = 3;
+/* Unit system wait time to complete join procedure */
+#define JOIN_DELAY      (10 * US_PER_SEC)
 
-int _send(void)
+/* Sleep time in seconds */
+#define DELAY           (30 * US_PER_SEC)
+
+static cayenne_lpp_t lpp;
+
+int _send(const kernel_pid_t *interface)
 {
     gnrc_pktsnip_t *pkt;
-    char temp_s[]={"250"};
+    msg_t msg;
 
-    uint8_t port = CONFIG_LORAMAC_DEFAULT_TX_PORT; /* Default: 2 */
+    if (!(pkt = gnrc_pktbuf_add(NULL, lpp.buffer, lpp.cursor,
+                                GNRC_NETTYPE_UNDEF))) {
+        LOG_INFO("No space in packet buffer\n");
+        return 1;
+    }
 
-    pkt = gnrc_pktbuf_add(NULL, temp_s, strlen(temp_s), GNRC_NETTYPE_UNDEF);
+    /* Reset Cayenne lpp buffer and reset the cursor */
+    cayenne_lpp_reset(&lpp);
 
-    /* register for returned packet status */
+    /* Register for returned packet status */
     if (gnrc_neterr_reg(pkt) != 0) {
-        puts("Can not register for error reporting");
+        LOG_INFO("Can not register for error reporting\n");
         return 0;
     }
-    
-    gnrc_netapi_set(interface, NETOPT_LORAWAN_TX_PORT, 0, &port, sizeof(port));
 
-    gnrc_netif_send(gnrc_netif_get_by_pid(interface), pkt); /* Forward the packet to LoRaWAN MAC for Uplink*/
+    /* Forward the packet to LoRaWAN MAC for Uplink*/
+    gnrc_netif_send(gnrc_netif_get_by_pid(*interface), pkt);
 
-    /* wait for packet status and check */
-    msg_t msg;
+    /* Wait for packet status and check */
     msg_receive(&msg);
+
     if ((msg.type != GNRC_NETERR_MSG_TYPE) ||
         (msg.content.value != GNRC_NETERR_SUCCESS)) {
-        printf("Error sending packet: (status: %d\n)", (int) msg.content.value);
+        LOG_INFO("Error sending packet: (status: %d\n)",
+                  (int) msg.content.value);
     }
     else {
-        puts("Successfully sent packet");
+        LOG_INFO("Successfully sent packet\n");
     }
 
     return 0;
 }
 
-static int _sense(char *temp)
+static int _sense(void)
 {
     phydat_t res;
+
     saul_reg_t *dev = saul_reg;
-
     if (dev == NULL) {
-            puts("No SAUL devices present");
-            return 1;
-        }
+        LOG_INFO("No SAUL devices present. Exit... \n");
+        assert(false);
+        return 1;
+    }
 
+    /* TODO: Use saul_reg_find_xxx functions */
     while (dev) {
         int dim = saul_reg_read(dev, &res);
-        if (strcmp(saul_class_to_str(dev->driver->type), "ACT_SWITCH") &&
-            strcmp(saul_class_to_str(dev->driver->type), "SENSE_BTN"))
-            {   
-                printf("\nDev: %s\tType: %s\n", dev->name,
-                        saul_class_to_str(dev->driver->type));
-                phydat_dump(&res, dim); 
-                printf("Dataa : %d, scale %d\n", res.val[0], res.scale);
-                temp = res.val[0];
+        switch (dev->driver->type) {
+            case SAUL_SENSE_TEMP :
+                phydat_dump(&res, dim);
+                cayenne_lpp_add_temperature(&lpp, 3, res.val[0]/10);
+                break;
+            case SAUL_SENSE_HUM :
+                phydat_dump(&res, dim);
+                cayenne_lpp_add_relative_humidity(&lpp, 4, res.val[0]);
+                break;
             }
-                dev = dev->next;
+
+            dev = dev->next;
         }
-    size_t n = fmt_s16_dfp(temp_s, temp, -1);
-    temp_s[n] = '\0';
-}   
-static int _join(void)
+        return 0;
+}
+
+/* Join the network */
+static netopt_enable_t _join(const kernel_pid_t *interface)
 {
-    uint8_t enable = 1;
-    int status = 0;
-    gnrc_netapi_set(interface, NETOPT_LINK, 0, &enable, sizeof(enable));
-    xtimer_usleep(DELAY);
-    gnrc_netapi_get(interface, NETOPT_LINK, 0, &status, sizeof(status));
-    return status;
+    netopt_enable_t en = NETOPT_ENABLE;
+    gnrc_netapi_set(*interface, NETOPT_LINK, 0, &en, sizeof(en));
+    xtimer_usleep(JOIN_DELAY);
+    gnrc_netapi_get(*interface, NETOPT_LINK, 0, &en, sizeof(en));
+    return en;
+}
+
+/* Get interface ID */
+static int _get_interface(kernel_pid_t *interface)
+{
+    gnrc_netif_t *netif = NULL;
+
+    /* Iterate over all network interfaces */
+    while ((netif = gnrc_netif_iter(netif))) {
+        int _type;
+        gnrc_netapi_get(netif->pid, NETOPT_DEVICE_TYPE, 0, &_type,
+                        sizeof(_type));
+        if (_type == NETDEV_TYPE_LORA) {
+            *interface = netif->pid;
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int main(void)
 {
-    puts("LoRaWAN SAUL test application\n");
-    char temp[];
+    LOG_INFO("LoRaWAN SAUL test application\n");
 
-    while(!_join()){} /* Wait for node to join NW */
-    puts("Device joined\n");
+    kernel_pid_t interface_d;
+    if(_get_interface(&interface_d)) {
+        LOG_INFO("Couldn't find a LoRaWAN interface");
+        return 1;
+    }
 
-    /* periodically read temp and humidity values */
+    /* Wait for node to join NW */
+    while(_join(&interface_d) != NETOPT_ENABLE) {}
+    LOG_INFO("Device joined\n");
+
+    /* Configure GNRC LoRaWAN using GNRC NetAPI */
+    uint8_t port = CONFIG_LORAMAC_DEFAULT_TX_PORT; /* Default: 2 */
+
+    gnrc_netapi_set(interface_d, NETOPT_LORAWAN_TX_PORT, 0, &port,
+                    sizeof(port));
+
+    /* Periodically poll sensor value(s) and transmit */
     while (1) {
-        _sense(temp)
-        _send();
-         xtimer_usleep(DELAY);
+        _sense();
+        _send(&interface_d);
+        xtimer_usleep(DELAY);
     }
 
     return 0;
