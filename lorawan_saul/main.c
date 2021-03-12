45144fcaab6c02079ac80fd6d240d6967c78b80b
Jose Alamos <jose@alamos.cc>
2021-03-12 15:48:35 +0100

    improve some parts

--- lorawan_saul/main.c
+++ lorawan_saul/main.c
@@ -26,19 +26,11 @@
 #include "thread.h"
 #include "xtimer.h"
 
-// #include "timex.h"
-// #include "fmt.h"
-// #include "dht.h"
-// #include "dht_params.h"
-
 #include "phydat.h"
 #include "saul_reg.h"
-#include <string.h>
 
-#define DELAY           (10 * US_PER_SEC)
-#define MODULE_DHT11
-
-#define MSG           "{ '%s': [ { 'ts': %llu000, 'values':{'%s': %d, '%s': %d}}]}"
+#include "timex.h"
+#include "fmt.h"
 
 #include "board.h"
 
@@ -47,22 +39,26 @@
 
 #include "net/gnrc/pktbuf.h"
 #include "net/gnrc/pktdump.h"
-#include "net/gnrc/netreg.h"
+
 #include "net/loramac.h"
 
+#define DELAY           (10 * US_PER_SEC)
 #define LORAWAN_QUEUE_SIZE (4U)
 
-// static gnrc_netif_t _netif;
 static uint8_t interface = 3;
 
 int _send(void)
 {
     gnrc_pktsnip_t *pkt;
+
+    msg_t msg;
     char temp_s[]={"250"};
+    /* TODO: Replace by cayenne sensor */
 
-    uint8_t port = CONFIG_LORAMAC_DEFAULT_TX_PORT; /* Default: 2 */
-
-    pkt = gnrc_pktbuf_add(NULL, temp_s, strlen(temp_s), GNRC_NETTYPE_UNDEF);
+    if (!(pkt = gnrc_pktbuf_add(NULL, temp_s, strlen(temp_s), GNRC_NETTYPE_UNDEF))) {
+        puts("No space in packet buffer");
+        return 1;
+    }
 
     /* register for returned packet status */
     if (gnrc_neterr_reg(pkt) != 0) {
@@ -70,13 +66,12 @@ int _send(void)
         return 0;
     }
     
-    gnrc_netapi_set(interface, NETOPT_LORAWAN_TX_PORT, 0, &port, sizeof(port));
-
-    gnrc_netif_send(gnrc_netif_get_by_pid(interface), pkt); /* Forward the packet to LoRaWAN MAC for Uplink*/
+    /* Forward the packet to LoRaWAN MAC for Uplink*/
+    gnrc_netif_send(gnrc_netif_get_by_pid(interface), pkt);
 
     /* wait for packet status and check */
-    msg_t msg;
     msg_receive(&msg);
+
     if ((msg.type != GNRC_NETERR_MSG_TYPE) ||
         (msg.content.value != GNRC_NETERR_SUCCESS)) {
         printf("Error sending packet: (status: %d\n)", (int) msg.content.value);
@@ -88,18 +83,21 @@ int _send(void)
     return 0;
 }
 
-static int _sense(char *temp)
+static int _sense(void)
 {
     phydat_t res;
+
     saul_reg_t *dev = saul_reg;
-
     if (dev == NULL) {
-            puts("No SAUL devices present");
-            return 1;
-        }
+        puts("No SAUL devices present. Exit...");
+        assert(false);
+        return 1;
+    }
 
+    /* TODO: Use saul_reg_find_xxx functions */
     while (dev) {
         int dim = saul_reg_read(dev, &res);
+        /* TODO: Add the temperature and humidity sensors */
         if (strcmp(saul_class_to_str(dev->driver->type), "ACT_SWITCH") &&
             strcmp(saul_class_to_str(dev->driver->type), "SENSE_BTN"))
             {   
@@ -107,34 +105,39 @@ static int _sense(char *temp)
                         saul_class_to_str(dev->driver->type));
                 phydat_dump(&res, dim); 
                 printf("Dataa : %d, scale %d\n", res.val[0], res.scale);
-                temp = res.val[0];
+                //temp = res.val[0];
+                /* TODO: Load cayenne buffer */
             }
                 dev = dev->next;
         }
-    size_t n = fmt_s16_dfp(temp_s, temp, -1);
-    temp_s[n] = '\0';
+    return 0;
 }   
-static int _join(void)
+
+static netopt_enable_t _join(void)
 {
-    uint8_t enable = 1;
-    int status = 0;
-    gnrc_netapi_set(interface, NETOPT_LINK, 0, &enable, sizeof(enable));
+    netopt_enable_t en = NETOPT_ENABLE;
+    gnrc_netapi_set(interface, NETOPT_LINK, 0, &en, sizeof(en));
     xtimer_usleep(DELAY);
-    gnrc_netapi_get(interface, NETOPT_LINK, 0, &status, sizeof(status));
-    return status;
+    gnrc_netapi_get(interface, NETOPT_LINK, 0, &en, sizeof(en));
+    return en;
 }
 
 int main(void)
 {
     puts("LoRaWAN SAUL test application\n");
-    char temp[];
+    //char temp[];
 
-    while(!_join()){} /* Wait for node to join NW */
+    while(_join() != NETOPT_ENABLE) {} /* Wait for node to join NW */
     puts("Device joined\n");
+
+    /* Configure GNRC LoRaWAN using GNRC NetAPI */
+    uint8_t port = CONFIG_LORAMAC_DEFAULT_TX_PORT; /* Default: 2 */
+
+    gnrc_netapi_set(interface, NETOPT_LORAWAN_TX_PORT, 0, &port, sizeof(port));
 
     /* periodically read temp and humidity values */
     while (1) {
-        _sense(temp)
+        _sense();
         _send();
          xtimer_usleep(DELAY);
     }
